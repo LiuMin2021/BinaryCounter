be0a29fa2ab2e7be28d4c7380ff7e1a5910c244a
LiuMin2021 <min.liu@bulme.at>
2022-06-12 18:05:11 +0100

    Abgabe

--- counter.py
+++ counter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # 2021 nr@bulme.at
 
-from gpiozero import Button
+from gpiozero import LEDBoard, Button
 from PyQt5.QtCore import Qt, QObject, pyqtSignal
 from PyQt5.QtWidgets import (QWidget, QLCDNumber,
     QVBoxLayout, QApplication)
@@ -9,6 +9,13 @@ from PyQt5.QtWidgets import (QWidget, QLCDNumber,
 DOWN_PIN = 22
 RESET_PIN = 27
 UP_PIN = 17 
+
+LED0 = 18
+LED1 = 23
+LED2 = 24
+LED3 = 25
+
+
 
 class QtButton(QObject):
     changed = pyqtSignal()
@@ -26,6 +33,10 @@ class Counter(QWidget):
         super().__init__()
         self.initUi()
         self.count = 0
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.D = 0
 
     def initUi(self):
         self.lcd = QLCDNumber()
@@ -42,7 +53,55 @@ class Counter(QWidget):
 
     def countUp(self):
         self.count += 1
+        self.D += 1
+        if self.D==2:
+            self.D=0
+            self.C+=1
+        if self.C==2:
+            self.C=0
+            self.B+=1
+        if self.B==2:
+            self.B=0
+            self.A=1
+        if self.count == 16:
+            self.count = 0
+            self.A = 0
+            self.B = 0
+            self.C = 0
+            self.D = 0
         self.lcd.display(self.count)
+        leds.value = (self.A, self.B, self.C, self.D)
+        
+    def countDown(self):
+        self.count -= 1
+        self.D -= 1
+        if self.D == -1:
+            self.D=1
+            self.C-=1
+        if self.C == -1:
+            self.C=1
+            self.B-=1
+        if self.B == -1:
+            self.B=1
+            self.A=0
+        if self.count == -1:
+            self.count = 15
+            self.A = 1
+            self.B = 1
+            self.C = 1
+            self.D = 1
+        self.lcd.display(self.count)
+        leds.value = (self.A, self.B, self.C, self.D)
+        
+    def countReset(self):
+        self.count = 0
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.D = 0
+        self.lcd.display(self.count)
+        leds.value = (self.A, self.B, self.C, self.D)
+
  
 
 if __name__ ==  '__main__':
@@ -50,5 +109,10 @@ if __name__ ==  '__main__':
     gui = Counter()
     button = QtButton(UP_PIN)
     button.changed.connect(gui.countUp)
+    button = QtButton(DOWN_PIN)
+    button.changed.connect(gui.countDown)    
+    button = QtButton(RESET_PIN)
+    button.changed.connect(gui.countReset)
+    leds = LEDBoard(LED0, LED1, LED2, LED3)
     app.exec_()
     
